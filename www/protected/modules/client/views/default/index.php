0f6b5f065ede4ce0803706f53a461f4ad6322f7b
123423424 <vvv-v@mail.ru>
2014-09-24 07:35:06 +0700

    сделал пункты перелистывания заказов.

--- www/protected/modules/client/views/default/index.php
+++ www/protected/modules/client/views/default/index.php
@@ -17,13 +17,26 @@
 
 </main>
 <?php
-	echo Yii::app()->user->name.'='.Yii::app()->user->id['id'].'='.Yii::app()->user->id['Name'];
+    $ordersPage = 10; //Количество заказов на странице
+    $boocs = 5; //Всего книг на странице
+    
+    $page = abs((integer)Yii::app()->getRequest()->getQuery('page'));
+    $nowPage = $page - 1;  
+	
+    
     
     //Заказы
     $orders = Yii::app()->db->createCommand()
         ->select(array('id', 'Date', 'type', 'type_other', 'topic', 'item', 'datepicker', 'sources', 'status', 'mess'))
-        ->from('{{orders}}')->where('idClient="'.Yii::app()->user->id['id'].'"')->order('mess DESC, Date DESC') -> queryAll(); 
-    
+        ->from('{{orders}}')->where('idClient="'.Yii::app()->user->id['id'].'"')->order(' Date DESC') -> queryAll(); 
+        $allOrders =  count($orders);
+     $orders = Yii::app()->db->createCommand()
+        ->select(array('id', 'Date', 'type', 'type_other', 'topic', 'item', 'datepicker', 'sources', 'status', 'mess'))
+        ->from('{{orders}}')->where('idClient="'.Yii::app()->user->id['id'].'"')->order('id  ') -> 
+        limit($ordersPage /* количесвто */, $nowPage*$ordersPage /* начиная с */) ->  queryAll(); 
+          
+        
+    //  mess DESC, Date DESC
 ?>
 
 <div class="bs-docs-featurette">
@@ -40,9 +53,7 @@
 			<tbody>
             
             <?php
-	foreach ($orders as $a => $order ) {
-	           
-             
+	foreach ($orders as $a => $order ) {   
              echo '<tr data-nomer = "'.$order['id'].'" >
                     <td>'.$order['id'].' '.$order['item'].'. Тема:'.$order['topic'].' ('; 
              echo  $order['type'] == 'другое' ?  $order['type_other'] : $order['type'];  
@@ -52,14 +63,53 @@
 						<span class="glyphicon glyphicon-envelope f-size30 ';
              echo  $order['mess'] == 1 ?  'chengeColor' : '';             
       
-             echo '"></span>
-                        
-					</td></tr>';
-               
+             echo '"></span>                        
+					</td></tr>';               
         }
 ?>
-
 			</tbody>
 		</table>
+  <?php
+	$allBoocs =  ceil($allOrders/$ordersPage); //всего книг (округление в большую сторуну) 
+    if ($page == 0) $page = 1;    
+    $booc = ceil($page/$boocs) -1; //Окрулгение в меньшую           
+           $f =  ($booc+1)*$boocs;          
+           if ($f < $allBoocs ) { $iEnd =  $f;} else { $iEnd =  $allBoocs;}
+           
+    
+    if ($allBoocs > 1) {
+        $pDown = $booc*$boocs;
+        $pUp = ($booc+1)*$boocs+1; 
+       echo  '<ul class="pagination">';
+      
+       
+            if ($booc==0){
+                        echo '<li class="disabled"><a href="">&laquo;</a></li>';                    
+                    } else {
+                        echo '<li><a href="?page='.$pDown.'">&laquo;</a></li>'; 
+                    }       
+               
+           for ($i = $booc*$boocs+1; $i <= $iEnd; $i++) {
+                if ($page == $i ) {
+                    echo '<li class="active"><a href="#">'.$i.'<span class="sr-only">(current)</span>';
+                } else {
+                    echo '<li><a href="?page='.$i.'">'.$i.'</a></li>';
+                }
+            }
+              
+              if ($f < $allBoocs ){
+                echo '<li><a href="?page='.$pUp.'">&raquo;</a></li>'; 
+                                          
+                    } else {
+                        echo '<li class="disabled"><a href="">&raquo;</a></li>';  
+                    }      
+       echo  '</ul>';
+        
+    }
+?>      
+     
+
 	</div>
 </div>
+
+
